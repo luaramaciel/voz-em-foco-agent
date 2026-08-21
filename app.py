35a71948d17a522bfd66e9437362d79fd24fb9e8
luaramaciel <luara.cadastros.com>
2026-08-21 15:43:19 -0300

    fix: corrige imports do LangChain no Streamlit

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # ==============================================================================
